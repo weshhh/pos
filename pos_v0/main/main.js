eb2037410e296ca2bdcf16bbf9e96d6f994befd9
weshhh <215205840@qq.com>
2016-04-19 14:48:24 +0800

    Update main.js
    change:
       total : totalmoney

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,4 +1,44 @@
-//TODO: Please write code in this file.
-function printReceipt(inputs) {
+function printReceipt(Items) {
+
+  var cartItems = buildCartItems(Items);
+  var receiptList = buildReceiptList(cartItems);
+  console.log(receiptList);
 
 }
+
+
+function buildCartItems(Items) {
+
+  var cartItems = [];
+
+  Items.forEach(function (element) {
+    cartItems.push({item: element, subTotal: element.count * element.price   })
+  });
+
+  return cartItems;
+}
+
+
+
+function buildReceiptList(cartItems) {
+
+  var totalMoney = 0;
+  cartItems.forEach(function(element) {
+    totalMoney += element.subTotal;
+  });
+
+
+  var finalCartItems = [];
+  finalCartItems.push({cartItems: cartItems, total:totalMoney});
+
+  var receiptList = '***<没钱赚商店>收据***\n';
+
+  cartItems.forEach(function (element) {
+    receiptList += '名称：' + element.item.name + '，数量：' + element.item.count + element.item.unit + '，单价：' + element.item.price.toFixed(2) + '(元)，小计：' + element.subTotal.toFixed(2) + '(元)\n';
+  });
+  receiptList += '----------------------\n' + '总计：' + finalCartItems.total + '(元)\n' + '**********************';
+
+  return receiptList;
+}
+
+
